b8316311a90407406f83c96177e0809df0a89fdb
yucelcubukk <yucelcubuk30@gmail.com>
2025-01-22 16:55:38 +0300

    İlk commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React Router Kurulumu</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/global.css";
+import AppRouter from "./router/AppRouter";
+
+function App() {
+  return <AppRouter />;
+}
+
+export default App;
+
